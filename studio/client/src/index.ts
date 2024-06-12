72da8cfd9f6cb02dd872703e40f2be02836ddca2
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2024-06-12 15:25:56 +0200

    fixed studio client export

--- studio/client/src/index.ts
+++ studio/client/src/index.ts
@@ -1,3 +1,4 @@
 export * from './client.js';
 export * from './InteractionBase.js';
 export type { FilterOption } from './RunsApi.js';
+export type { ListInteractionsResponse } from './PromptsApi.js';

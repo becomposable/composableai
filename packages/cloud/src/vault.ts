2799f445f9dc79c52f2423bccff8a1f53d59c0e1
Eric Barroca <eric@composable.sh>
2024-08-23 12:53:08 +0900

    chore: add debug log for inspection

--- packages/cloud/src/vault.ts
+++ packages/cloud/src/vault.ts
@@ -1,15 +1,23 @@
 import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
+import { GoogleAuth } from 'google-auth-library';
 
 
 export class VaultClient {
 
 
     async getSecret(secretName: string) {
+        0
+        console.log(`Getting secret ${secretName} from vault`);
+        const auth = new GoogleAuth({
+            scopes: ['https://www.googleapis.com/auth/cloud-platform']
+        });
+        const authClient = await auth.getClient();
+        console.log('Credential type:', authClient.constructor.name);
+        console.log('Credential token:', authClient.credentials.id_token);
 
         const client = new SecretManagerServiceClient();
-        console.log(`Getting secret ${secretName} from vault`);
 
-        
+        console.log('Getting secret version');
         const [version] = await client.accessSecretVersion({
             name: `projects/${process.env.GOOGLE_PROJECT_ID}/secrets/${secretName}/versions/latest`,
         });
@@ -20,7 +28,7 @@ export class VaultClient {
         }
 
         if (typeof secret !== 'string') {
-            return (secret as Buffer).toString('utf-8')        
+            return (secret as Buffer).toString('utf-8')
         } else {
             return secret
         }

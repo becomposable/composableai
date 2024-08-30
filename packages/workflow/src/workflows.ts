1ffc6130f29e1cda5ff64d5b6e24572a7c3e10fa
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2024-08-30 16:56:34 +0200

    moved cloud and workflow to studio

--- packages/workflow/src/workflows.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Export workflows to be registered on temporal workers
- */
-export { dslWorkflow } from "./dsl/dsl-workflow.js";
-export { recalculateEmbeddingsWorkflow } from "./system/recalculateEmbeddingsWorkflow.js";
-

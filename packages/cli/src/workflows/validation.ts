9fe95e559c259601c8ba8f861359b923fcb159f8
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2024-09-18 14:38:11 +0200

    #311 - restored workflow validation in cli

--- packages/cli/src/workflows/validation.ts
+++ packages/cli/src/workflows/validation.ts
@@ -1,4 +1,4 @@
-//import { validateWorkflow as validate } from "@becomposable/workflow/vars";
+import { validateWorkflow as validate } from "@becomposable/workflow/vars";
 import { DSLWorkflowSpec } from "@becomposable/common";
 export class ValidationError extends Error {
     constructor(message: string) {
@@ -7,23 +7,17 @@ export class ValidationError extends Error {
     }
 }
 
-//TODO move validation code from workflow to a public project
-// export function validateWorkflow(spec: DSLWorkflowSpec): DSLWorkflowSpec {
-//     const errors = validate(spec);
-//     if (errors && errors.length > 0) {
-//         console.log("");
-//         console.log("# ");
-//         console.log("# The workflow has validation errors:");
-//         console.log("# ");
-//         console.log("");
-//         console.log(errors.map((v, i) => `${i + 1}. ${v}`).join("\n"));
-//         console.log("");
-//         throw new ValidationError("Exiting because of validation errors.");
-//     }
-//     return spec;
-// }
-
 export function validateWorkflow(spec: DSLWorkflowSpec): DSLWorkflowSpec {
-    // do nothing for now
+    const errors = validate(spec);
+    if (errors && errors.length > 0) {
+        console.log("");
+        console.log("# ");
+        console.log("# The workflow has validation errors:");
+        console.log("# ");
+        console.log("");
+        console.log(errors.map((v, i) => `${i + 1}. ${v}`).join("\n"));
+        console.log("");
+        throw new ValidationError("Exiting because of validation errors.");
+    }
     return spec;
 }

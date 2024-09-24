a149a5f80c3b8fb0de056e58ea589179edfbb63b
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2024-09-24 16:13:40 +0200

    #266 - wip - better error reporting

--- packages/memory-cli/examples/release-notes.ts
+++ packages/memory-cli/examples/release-notes.ts
@@ -8,19 +8,17 @@ if (!start || !end) {
 
 console.log(`Retrieving issues between ${start}and ${end}...`)
 // Get list of commit logs containing '#' between the two tags and extract unique issue numbers
-const issue_numbers = await exec(`git log ${start}..${end} --oneline | grep -o '#[0-9]\+'`);// | sed 's/#//' | sort -u`) as string;
+const issue_numbers = await exec(`git log ${start}..${end} --oneline | grep -o '#[0-9]\\+' | sed 's/#//' | sort -u`) as string;
 
-console.log('##########issues are:', issue_numbers)
+for (const issue of issue_numbers!.trim().split("\n")) {
+    console.log(`Processing issue #${issue}`)
+    const issue_content = await exec(`gh issue view ${issue}`) as string;
+    copyText(issue_content!, `issues/${issue}.txt`);
+}
 
-// for (const issue of issue_numbers!.trim().split("\n")) {
-//     console.log(`Processing issue #${issue}`)
-//     const issue_content = await exec(`gh issue view ${issue}`) as string;
-//     copyText(issue_content!, `issues/${issue}.txt`);
-// }
-
-// console.log("Generating diff");
-// await exec(`git diff --submodule=diff ${start}...${end} > ${tmpdir}/range_diff.txt`)
-// copy(`${tmpdir}/range_diff.txt`, "range_diff.txt");
+console.log("Generating diff");
+await exec(`git diff --submodule=diff ${start}...${end} > ${tmpdir}/range_diff.txt`)
+copy(`${tmpdir}/range_diff.txt`, "range_diff.txt");
 
 // no metadata to export
 export default {}

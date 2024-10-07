8aa6e62a023bc27c236b423f224e20dd20780c91
Mincong Huang <mhuang@becomposable.com>
2024-10-07 12:14:13 +0200

    Fix pandoc test

--- packages/converters/src/pandoc.test.ts
+++ packages/converters/src/pandoc.test.ts
@@ -2,13 +2,13 @@ import fs from 'fs';
 import path from 'path';
 import { expect, test } from 'vitest';
 import { manyToMarkdown } from './pandoc';
-
+import { Readable } from 'stream';
 
 
 
 // Add more test cases for other file types (ODT, DOCX) if needed
 test('should convert docx to markdown', async () => {
-  const docx = fs.readFileSync(path.join(__dirname, '../fixtures', 'us-ciia.docx'));
-  const result = await manyToMarkdown(Buffer.from(docx), 'docx');
+  const docx: Buffer = fs.readFileSync(path.join(__dirname, '../fixtures', 'us-ciia.docx'));
+  const result = await manyToMarkdown(Readable.from(docx), 'docx');
   expect(result).to.include('confidential');
 });

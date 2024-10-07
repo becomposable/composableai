bfa35ca386c7303b14c408201a37366971676d63
Mincong Huang <mhuang@becomposable.com>
2024-10-07 12:08:50 +0200

    Install mutool

--- packages/converters/src/mutool.test.ts
+++ packages/converters/src/mutool.test.ts
@@ -1,14 +1,14 @@
 import fs from 'fs';
 import path from 'path';
 import { expect, test } from 'vitest';
-import { mutoolPdfToText } from './mutool.js';
+import { pdfToText } from './mutool.js';
 
 
 test('[mutool] should convert pdf to text', async () => {
   const pdf = fs.readFileSync(path.join(__dirname, '../fixtures', 'test-pdf2.pdf'));
   const buf = Buffer.from(pdf);
   console.log("Running mutoolPdfToText")
-  const result = mutoolPdfToText(buf);
+  const result: string = await pdfToText(buf);
   expect(result).toContain('its attentive Ambassadors');
 
 });

8acdb0a9d18c6e15169fe99aecc06ef8d4940a41
Eric Barroca <eric@composable.sh>
2024-10-18 16:09:48 +0900

    fix: remove prompt from interactionrefselect

--- packages/common/src/interaction.ts
+++ packages/common/src/interaction.ts
@@ -232,7 +232,7 @@ export interface ExecutionRunRef
     interaction: InteractionRef;
 }
 
-export const ExecutionRunRefSelect = "-result -parameters -result_schema";
+export const ExecutionRunRefSelect = "-result -parameters -result_schema -prompt";
 
 export interface InteractionExecutionConfiguration {
     environment?: string;

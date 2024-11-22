fad558c1947e1d279f8e4655e6cc8eedba0e97a7
Leon Ruggiero <lruggiero@becomposable.com>
2024-11-22 14:28:13 +0000

    Update ParentClosePolicy to match new temporalio version

--- packages/common/src/store/temporalio.ts
+++ packages/common/src/store/temporalio.ts
@@ -1,25 +1,41 @@
-
-/**
- * Copied from temporalio
- * The temporalio ParentClosePolicy
- */
-export enum ParentClosePolicy {
-    /**
-     * If a `ParentClosePolicy` is set to this, or is not set at all, the server default value will be used.
-     */
-    PARENT_CLOSE_POLICY_UNSPECIFIED = 0,
+export type ParentClosePolicy = (typeof ParentClosePolicy)[keyof typeof ParentClosePolicy];
+export declare const ParentClosePolicy: {
     /**
      * When the Parent is Closed, the Child is Terminated.
      *
      * @default
      */
-    PARENT_CLOSE_POLICY_TERMINATE = 1,
+    readonly TERMINATE: "TERMINATE";
     /**
      * When the Parent is Closed, nothing is done to the Child.
      */
-    PARENT_CLOSE_POLICY_ABANDON = 2,
+    readonly ABANDON: "ABANDON";
     /**
      * When the Parent is Closed, the Child is Cancelled.
      */
-    PARENT_CLOSE_POLICY_REQUEST_CANCEL = 3
-}
+    readonly REQUEST_CANCEL: "REQUEST_CANCEL";
+    /**
+     * If a `ParentClosePolicy` is set to this, or is not set at all, the server default value will be used.
+     *
+     * @deprecated Either leave property `undefined`, or set an explicit policy instead.
+     */
+    readonly PARENT_CLOSE_POLICY_UNSPECIFIED: undefined;
+    /**
+     * When the Parent is Closed, the Child is Terminated.
+     *
+     * @deprecated Use {@link ParentClosePolicy.TERMINATE} instead.
+     */
+    readonly PARENT_CLOSE_POLICY_TERMINATE: "TERMINATE";
+    /**
+     * When the Parent is Closed, nothing is done to the Child.
+     *
+     * @deprecated Use {@link ParentClosePolicy.ABANDON} instead.
+     */
+    readonly PARENT_CLOSE_POLICY_ABANDON: "ABANDON";
+    /**
+     * When the Parent is Closed, the Child is Cancelled.
+     *
+     * @deprecated Use {@link ParentClosePolicy.REQUEST_CANCEL} instead.
+     */
+    readonly PARENT_CLOSE_POLICY_REQUEST_CANCEL: "REQUEST_CANCEL";
+};

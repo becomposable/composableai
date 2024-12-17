97bf34321d5cd2d011a4646bad033307df555995
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2024-12-17 21:02:02 +0100

    #77 - fixed ParentClosePolicy definition

--- packages/cli/src/workflows/index.ts
+++ packages/cli/src/workflows/index.ts
@@ -10,28 +10,28 @@ export function registerWorkflowsCommand(program: Command) {
         .option('--name [name]', 'The name of the workflow definition to create.')
         .option('--on [event]', 'The event which trigger this workflow. Format: "eventName[:objectType]" where objectType is optional.')
         .option('--run [endpoint]', 'The workflow to run.')
-        .action((options: Record<string, any>) => {
-            createWorkflowRule(program, options);
+        .action(async (options: Record<string, any>) => {
+            await createWorkflowRule(program, options);
         });
 
     rules.command("get <workflowId>")
         .description("Get a workflow given its ID")
         .option('-f, --file [file]', 'The file to save the workflow definition to.')
-        .action((workflowId: string, options: Record<string, any>) => {
-            getWorkflowRule(program, workflowId, options);
+        .action(async (workflowId: string, options: Record<string, any>) => {
+            await getWorkflowRule(program, workflowId, options);
         });
 
     rules.command("apply")
         .description("Update a workflow a workflow given its ID")
         .option('-f, --file <file>', 'The file to save the workflow definition to.')
-        .action((options: Record<string, any>) => {
-            createOrUpdateWorkflowRule(program, options);
+        .action(async (options: Record<string, any>) => {
+            await createOrUpdateWorkflowRule(program, options);
         });
 
     rules.command("list")
         .description("List all workflows")
-        .action((options: Record<string, any>) => {
-            listWorkflowsRule(program, options);
+        .action(async (options: Record<string, any>) => {
+            await listWorkflowsRule(program, options);
         });
 
     rules.command("execute <workflowId>")
@@ -39,14 +39,14 @@ export function registerWorkflowsCommand(program: Command) {
         .option('-o, --objectId [objectIds...]', 'The object to execute the workflow on.')
         .option('--vars [vars]', 'workflow vars as an inlined JSON string.')
         .option('-f, --file [file]', 'The file containing workflow execution payload.')
-        .action((workflowId: string, options: Record<string, any>) => {
-            executeWorkflowRule(program, workflowId, options);
+        .action(async (workflowId: string, options: Record<string, any>) => {
+            await executeWorkflowRule(program, workflowId, options);
         });
 
     rules.command("delete <objectId>")
         .description("Delete a workflow given its ID")
-        .action((objectId: string, options: Record<string, any>) => {
-            deleteWorkflowRule(program, objectId, options);
+        .action(async (objectId: string, options: Record<string, any>) => {
+            await deleteWorkflowRule(program, objectId, options);
         });
 
     workflows.command("execute <workflowName>")
@@ -54,49 +54,49 @@ export function registerWorkflowsCommand(program: Command) {
         .option('-o, --objectId [objectIds...]', 'Optional object to execute the workflow on.')
         .option('--vars', 'The workflow vars as an inline JSON.')
         .option('-f, --file [file]', 'A file containing workflow execution payload.')
-        .action((workflowName: string, options: Record<string, any>) => {
-            executeWorkflowByName(program, workflowName, options);
+        .action(async (workflowName: string, options: Record<string, any>) => {
+            await executeWorkflowByName(program, workflowName, options);
         });
     const definitions = workflows.command("definitions");
 
     definitions.command("transpile <files...>")
         .description("Transpile a typescript workflow definition to JSON.")
         .option('-o, --out [file]', 'An output file or directory. When multiple files are specified it must be a directory. If not specified the transpiled files are printed to stdoud.')
-        .action((files: string[], options: Record<string, any>) => {
-            transpileWorkflow(program, files, options);
+        .action(async (files: string[], options: Record<string, any>) => {
+            await transpileWorkflow(program, files, options);
         });
 
     definitions.command("create")
         .description("Create a new workflow definition.")
         .option('-f, --file <file>', 'The file containing the workflow definition.')
-        .action((options: Record<string, any>) => {
-            createOrUpdateWorkflowDefinition(program, undefined, options);
+        .action(async (options: Record<string, any>) => {
+            await createOrUpdateWorkflowDefinition(program, undefined, options);
         });
 
     definitions.command("apply [workflowId]")
         .description("Create or update a workflow definition using a file.")
         .option('-f, --file <file>', 'The file containing the workflow definition.')
         .option('--skip-validation', 'Skip the validation of the workflow definition.')
-        .action((workflowId, options: Record<string, any>) => {
-            createOrUpdateWorkflowDefinition(program, workflowId, options);
+        .action(async (workflowId, options: Record<string, any>) => {
+            await createOrUpdateWorkflowDefinition(program, workflowId, options);
         });
 
     definitions.command("list")
         .description("List all workflow definitions.")
-        .action((options: Record<string, any>) => {
-            listWorkflowsDefinition(program, options);
+        .action(async (options: Record<string, any>) => {
+            await listWorkflowsDefinition(program, options);
         });
 
     definitions.command("get <objectId>")
         .description("Get a workflow definition given its ID.")
         .option('-f, --file [file]', 'The file to save the workflow definition to.')
-        .action((objectId: string, options: Record<string, any>) => {
-            getWorkflowDefinition(program, objectId, options);
+        .action(async (objectId: string, options: Record<string, any>) => {
+            await getWorkflowDefinition(program, objectId, options);
         });
 
     definitions.command("delete <objectId>")
         .description("Delete a workflow definition given its ID.")
-        .action((objectId: string, options: Record<string, any>) => {
-            deleteWorkflowDefinition(program, objectId, options);
+        .action(async (objectId: string, options: Record<string, any>) => {
+            await deleteWorkflowDefinition(program, objectId, options);
         });
 }
